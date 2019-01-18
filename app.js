253f0c5a11b441ff51d45ae776ecba8a5ffcde1b
Daffanie Hurley <daffhur50@gmail.com>
2019-01-18 11:31:53 -0600

    Added config, initialize and serialize

--- app.js
+++ app.js
@@ -30,6 +30,43 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//~line 32 before routes
+app.use(require('express-session')({
+  //Define the session store
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection
+  }),
+  //Set the secret
+  secret: config.session.secret,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    path: '/',
+    domain: config.cookie.domain,
+    //httpOnly: true,
+    //secure: true,
+    maxAge:3600000 //1 hour
+  }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+//~line 53
+passport.serializeUser(function(user, done){
+  done(null,{
+    id: user._id,
+    username: user.username,
+    email: user.email,
+    first_name: user.first_name,
+    last_name: user.last_name
+  });
+});
+
+passport.deserializeUser(function(user, done){
+  done(null, user);
+});
+
 app.use('/', indexRouter);
 app.use('/api/users', apiUsersRouter);
 app.use('/users', usersRouter);
